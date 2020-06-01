5c29947395db5639470ec40ad9a443b071d95f30
Tendai Patrick Mamini <tendaimamini@gmail.com>
2020-06-02 01:06:53 +0200

    Update JadeCaseAlgo.java

--- jadenCase/JadeCaseAlgo.java
+++ jadenCase/JadeCaseAlgo.java
@@ -3,10 +3,11 @@ package jadenCase;
 public class JadeCaseAlgo {
 	public String toJadenCase (String phrase) {
 		if (phrase == null ||phrase.equals("")) {
-			return null;
+			return null;  //in put should be string or sentence
 		}
-	char [] array = phrase.toCharArray();
+	char [] array = phrase.toCharArray();// converting phrase string to a array of type char
 	
+		//iterate through the array checking the previous element if its "  " the the convert to upper case
 	for(int x = 0; x<array.length; x++) {
 		if (x==0 || array[x-1] == ' ' ) {
 			array[x] = Character.toUpperCase(array[x]);
@@ -14,7 +15,7 @@ public class JadeCaseAlgo {
 		}
 		
 	}
-		return new String(array);
+		return new String(array);// returning a string where all the first letters of each letter is a capital letter
 		
 	}
 
