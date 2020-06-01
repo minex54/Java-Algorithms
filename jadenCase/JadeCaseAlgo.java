a37d720b14877cd8531a18e7353b5cdef3813a17
minex <tendaimamini@gmail.com>
2020-06-01 20:23:55 +0200

    second

--- jadenCase/JadeCaseAlgo.java
+++ jadenCase/JadeCaseAlgo.java
@@ -1,10 +1,21 @@
 package jadenCase;
 
 public class JadeCaseAlgo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public String toJadenCase (String phrase) {
+		if (phrase == null ||phrase.equals("")) {
+			return null;
+		}
+	char [] array = phrase.toCharArray();
+	
+	for(int x = 0; x<array.length; x++) {
+		if (x==0 || array[x-1] == ' ' ) {
+			array[x] = Character.toUpperCase(array[x]);
+			
+		}
+		
+	}
+		return new String(array);
+		
 	}
 
 }
